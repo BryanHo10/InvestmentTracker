0d17e94998cb1d522cd88748abb9c9028693d2bf
Bryan Ho <bryankho1010@gmail.com>
2018-10-12 15:19:13 -0700

    added popup for extension

--- content.js
+++ content.js
@@ -1,7 +1,2 @@
-console.log("Stocky :)");
+console.log("Stocky");
 
-var paragraph = document.getElementsByTagName('p');
-
-for(var i =0;i<paragraph.length;i++){
-	paragraph[i].style['font-family']='Comic Sans MS';
-}
